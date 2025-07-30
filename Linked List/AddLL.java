94dc016cb1fcc3d3e238a0343b087ed5706e1f10
yuvraj <dhanal.yuvraj06@gmail.com>
2025-07-30 22:12:58 +0530

    Merge sort using ll added

--- Linked List/AddLL.java
+++ Linked List/AddLL.java
@@ -257,14 +257,75 @@ public class AddLL {
         }
     }
 
+    //Mergee sort in linked List
+
+    private Node getMid(Node head){
+        Node slow = head;
+        Node fast = head.next;
+        while(fast != null && fast.next!=null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+
+    private Node merge(Node head1, Node head2){
+        Node mergeLL = new Node(-1);
+        Node temp = mergeLL;
+        while(head1!=null && head2!=null){
+            if(head1.data <= head2.data){
+            temp.next = head1;
+            head1=head1.next;
+            temp=temp.next;
+            }else{
+                temp.next = head2;
+                head2 = head2.next;
+                temp = temp.next;
+            }
+        }
+
+        while(head1 != null){
+            temp.next = head1;
+            head1=head1.next;
+            temp=temp.next;
+        }
+
+        while(head2 != null){
+            temp.next = head2;
+            head2 = head2.next;
+            temp = temp.next;
+        }
+
+        return mergeLL.next;
+    }
+
+    public Node mergeSort(Node head){
+        //base case
+        if(head == null || head.next == null){
+            return head;
+        }
+        //find the mid
+        Node mid = getMid(head);
+
+        Node rightHead = mid.next;
+        mid.next = null;
+
+        Node newLeft = mergeSort(head);
+        Node newRight = mergeSort(rightHead);
+
+        return merge(newLeft, newRight);
+
+    }
+
     public static void main(String[] args) {
-        head = new Node(1);
-        Node temp = new Node(2);
-        head.next = temp;
-        head.next.next = new Node(3);
-        head.next.next.next = temp;
-        System.out.println(isCycle());
-        removeCycle();
-        System.out.println(isCycle());
+        AddLL ll = new AddLL();
+        ll.addFirst(1);
+        ll.addFirst(2);
+        ll.addFirst(3);
+        ll.addFirst(4);
+        ll.addFirst(5);
+        ll.print();
+        ll.head = ll.mergeSort(ll.head);
+        ll.print();
     }
 }
